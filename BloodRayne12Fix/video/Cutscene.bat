b8cd31ec72ef3101abcc89cf081a381c6b1a7d05
ShadowOfTheDark669 <155510064+ShadowOfTheDark669@users.noreply.github.com>
2025-09-14 01:54:33 +0530

    fixed critical file path typo

--- BloodRayne12Fix/video/Cutscene.bat
+++ BloodRayne12Fix/video/Cutscene.bat
@@ -1,1 +1,1 @@
-video/mpv/mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video/mpv/conf/input.conf --video-sync=display-resample %*
+video\mpv\mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video\mpv\conf\input.conf --video-sync=display-resample %*
