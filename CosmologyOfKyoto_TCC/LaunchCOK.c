d8197d98f79b2956928471525d218de7fbde77e2
ShadowOfTheDark669 <155510064+ShadowOfTheDark669@users.noreply.github.com>
2026-01-22 02:36:17 +0530

    Add files via upload

---
+++ CosmologyOfKyoto_TCC/LaunchCOK.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+ int main(){
+ char cmd[] = "cd DOSBox-X && dosbox-x.exe -conf ../dosbox_CosmologyOfKyoto.conf -noconsole";
+ system(cmd);
+
+
+
+
+
+  }
