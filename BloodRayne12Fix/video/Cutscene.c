eef02e87e9f6b6f6d0a98d29e901ee4017320a2a
ShadowOfTheDark669 <155510064+ShadowOfTheDark669@users.noreply.github.com>
2026-01-22 02:34:34 +0530

    Add files via upload

---
+++ BloodRayne12Fix/video/Cutscene.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    char cmd[200]; 
+    snprintf(cmd, 200, "video/mpv/mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video/mpv/conf/input.conf --video-sync=display-resample \"%s\"", argv[1]);
+    system(cmd);
+
+}   
