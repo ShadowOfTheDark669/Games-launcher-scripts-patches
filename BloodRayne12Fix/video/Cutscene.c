c04b4a356716c826fe071fe86c436eb23960cdf5
ShadowOfTheDark669 <155510064+ShadowOfTheDark669@users.noreply.github.com>
2026-01-22 03:38:30 +0530

    fixed paths

--- BloodRayne12Fix/video/Cutscene.c
+++ BloodRayne12Fix/video/Cutscene.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]) {
     char cmd[200]; 
-    snprintf(cmd, 200, "video/mpv/mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video/mpv/conf/input.conf --video-sync=display-resample \"%s\"", argv[1]);
+    snprintf(cmd, 200, ".\\video\\mpv\\mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video\\mpv\\conf\\input.conf --video-sync=display-resample \"%s\"", argv[1]);
     system(cmd);
 
 }   
