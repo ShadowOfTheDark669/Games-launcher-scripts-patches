eccbf260d4bbf0eb95ca0706b8f1d60243e1cec0
ShadowOfTheDark669 <155510064+ShadowOfTheDark669@users.noreply.github.com>
2026-02-11 01:10:03 +0530

    migrated to Win32Api

--- BloodRayne12Fix/video/Cutscene.c
+++ BloodRayne12Fix/video/Cutscene.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <windows.h>
 
-int main(int argc, char *argv[]) {
-    char cmd[200]; 
-    snprintf(cmd, 200, ".\\video\\mpv\\mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video\\mpv\\conf\\input.conf --video-sync=display-resample \"%s\"", argv[1]);
-    system(cmd);
+ int main(int argc, char *argv[]) {
 
-}   
+  char cmd[200];
+  snprintf(cmd, 200, ".\\video\\mpv\\mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video\\mpv\\conf\\input.conf --video-sync=display-resample \"%s\"", argv[1]);
+  
+
+  struct _STARTUPINFOA si = {0};
+  struct _PROCESS_INFORMATION pi = {0};
+
+  CreateProcessA(
+  NULL,
+  cmd, //cmdline
+  NULL, //pattr
+  NULL, //thattr
+  FALSE, //inherithandles
+  CREATE_NO_WINDOW,
+  NULL,
+  NULL,
+  &si,
+  &pi
+  	
+  );
+
+ 	
+ }
