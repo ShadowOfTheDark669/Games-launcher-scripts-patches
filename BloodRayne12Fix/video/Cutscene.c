fcb3a96dd4c27eb085e7b5b1b0d38b1012377559
ShadowOfTheDark669 <155510064+ShadowOfTheDark669@users.noreply.github.com>
2026-02-11 14:21:06 +0530

    locked mpv instances to one at a time to prevent playback of multiple files at once

--- BloodRayne12Fix/video/Cutscene.c
+++ BloodRayne12Fix/video/Cutscene.c
@@ -6,7 +6,7 @@
  int main(int argc, char *argv[]) {
 
   char cmd[200];
-  snprintf(cmd, 200, ".\\video\\mpv\\mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video\\mpv\\conf\\input.conf --video-sync=display-resample \"%s\"", argv[1]);
+  snprintf(cmd, 200, ".\\video\\mpv\\mpv.exe --fullscreen --osd-level=0 --no-osc --no-input-default-bindings --input-conf=video\\mpv\\conf\\input.conf --video-sync=display-resample --cursor-autohide=always \"%s\"", argv[1]);
   
 
   struct _STARTUPINFOA si = {0};
@@ -26,5 +26,12 @@
   	
   );
 
+  WaitForSingleObject(
+  pi.hProcess,
+  INFINITE
+  );
+
+  CloseHandle(pi.hProcess);
+  return 0;
  	
  }
